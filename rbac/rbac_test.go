2c4e09f7afda4320d2359db97a3ffc4ff41a9803
huoyinghui <yhhuo@yunjinginc.com>
2020-12-08 15:04:46 +0800

    update: log

--- rbac/rbac_test.go
+++ rbac/rbac_test.go
@@ -1,30 +1,46 @@
 package rbac
 
 import (
+	"github.com/casbin/casbin/v2"
 	"log"
 	"testing"
 )
 
-func testEnforce(t *testing.T, sub string, obj string, act string, res bool) {
+func testEnforce(t *testing.T, e  *casbin.Enforcer, sub string, obj string, act string, res bool) {
 	t.Helper()
-	if ok := check(authEnforcer, sub, obj, act); ok != res {
+	if ok := check(e, sub, obj, act); ok != res {
 		t.Errorf("%s, %v, %s: %t, supposed to be %t", sub, obj, act, ok, res)
 	}
 }
 
-func TestSetupRouter(t *testing.T) {
+
+func TestRBACV1(t *testing.T) {
 	// setup session store
+	authEnforcer, err := casbin.NewEnforcer(
+		"./conf/rbac_model_with_root.conf", "./conf/rbac_policy.csv")
+	if err != nil {
+		t.Error(err)
+	}
 	log.Printf("authEnforcer:%v", authEnforcer)
-	testEnforce(t, "alice", "data1", "write", false)
-	testEnforce(t,"alice", "data2", "write", true)
-	testEnforce(t,"bob", "data2", "read", false)
-	testEnforce(t,"bob", "data2", "write", true)
+	testEnforce(t, authEnforcer, "alice", "data1", "write", false)
+	testEnforce(t, authEnforcer,"alice", "data2", "write", true)
+	testEnforce(t, authEnforcer,"bob", "data2", "read", false)
+	testEnforce(t, authEnforcer,"bob", "data2", "write", true)
 	authEnforcer.AddRoleForUser("bob", "data2_admin")
-	testEnforce(t, "bob", "data2", "read", true)
+	testEnforce(t, authEnforcer, "bob", "data2", "read", true)
 	authEnforcer.DeleteRoleForUser("bob", "data2_admin")
-	testEnforce(t, "bob", "data2", "read", false)
+	testEnforce(t, authEnforcer, "bob", "data2", "read", false)
 	authEnforcer.AddPermissionForUser("bob", "data2", "read")
-	testEnforce(t, "bob", "data2", "read", true)
+	testEnforce(t, authEnforcer, "bob", "data2", "read", true)
 	authEnforcer.DeletePermissionForUser("bob", "data2", "read")
-	testEnforce(t,"bob", "data2", "read", false)
+	testEnforce(t, authEnforcer,"bob", "data2", "read", false)
+}
+
+func TestRBACWithRoot(t *testing.T)  {
+	authEnforcerWithRoot, err := casbin.NewEnforcer(
+		"./conf/rbac_model_with_root.conf", "./conf/rbac_policy.csv")
+	if err != nil {
+		t.Error(err)
+	}
+	testEnforce(t, authEnforcerWithRoot,"bob", "data2", "read", true)
 }
