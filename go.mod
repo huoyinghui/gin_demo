b5a88984bc21e78df2497839b9c2e2cb6ca15ad9
huoyinghui <yhhuo@yunjinginc.com>
2020-12-08 14:35:20 +0800

    add:casbin

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gin_demo
+
+go 1.13
+
+require (
+	github.com/casbin/casbin/v2 v2.19.4
+	github.com/gin-gonic/gin v1.6.3
+	github.com/stretchr/testify v1.6.1
+)
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"gin_demo/log"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
+func init() {
+}
+
+func main() {
+	r := SetupRouter()
+	r.Use(log.LogFormatter())
+	r.Run(":8080")
+}
